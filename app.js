be85a216314208d6d519d4e4733a323a8c6ff403
Austin Perry <austinjperry22@gmail.com>
2023-08-16 14:58:49 -0600

    refactored

--- app.js
+++ app.js
@@ -30,40 +30,18 @@ const toppings = [{
     quantity: 0
 }]
 
-function orderSprinkles() {
-    let sprinklesObj = toppings.find(topping => topping.name == 'Sprinkles')
-    sprinklesObj.quantity++
+
+function orderTopping(type) {
+    let toppingObj = toppings.find(topping => topping.name == type)
+    toppingObj.quantity++
+    drawCart()
 }
 
-function orderChocoChips() {
-    let chocoChipObj = toppings.find(topping => topping.name == 'Chocolate Chips')
-    chocoChipObj.quantity++
-}
-
-function orderGummyWorms() {
-    let gummyWormsObj = toppings.find(topping => topping.name == 'Gummy Worms')
-    gummyWormsObj.quantity++
-}
-
-function orderVanilla() {
-    console.log("ordering vanilla")
-    let vanillaObj = iceCream.find(icecream => icecream.name == 'Vanilla')
-    vanillaObj.quantity++
-    console.log(vanillaObj.quantity)
-}
-
-function orderChocolate() {
-    console.log("ordering Chocolate")
-    let chocolateObj = iceCream.find(icecream => icecream.name == 'Chocolate')
-    chocolateObj.quantity++
-    console.log(chocolateObj.quantity)
-}
-
-function orderStrawberry() {
-    console.log("ordering strawberry")
-    let strawberryObj = iceCream.find(icecream => icecream.name == 'Strawberry')
-    strawberryObj.quantity++
-    console.log(strawberryObj.quantity)
+function orderScoop(flavor) {
+    console.log("ordering " + flavor)
+    let scoopObj = iceCream.find(icecream => icecream.name == flavor)
+    scoopObj.quantity++
+    drawCart()
 }
 
 function drawTotal() {
